6a069fe33537a99ab8e5331dd2dc6bd61d64b4ff
tamomsi <122281591+tamomsi@users.noreply.github.com>
2023-02-20 18:09:52 +0100

    adding movies
    If applied this commit will add 10 movies to the file

--- index.js
+++ index.js
@@ -4,6 +4,49 @@ const app = express();
 
 const http = require('http');
 
+let topMovies = [ 
+    {
+        title: 'Titanic',
+        director: 'James Cameron',
+      },
+      {
+        title: 'Harry Potter and the Prisoner of Azkaban',
+        director: 'Alfonso Cuaron',
+      },
+      {
+        title: 'The Holiday',
+        director: 'Nancy Meyers',
+      },
+      {
+        title: 'Home for Christmas',
+        director: 'Per-Olav Sorensen',
+      },
+      {
+        title: 'Home Alone',
+        director: 'Chris Columbus',
+      },
+      {
+        title: 'Roman Holiday',
+        director: 'William Wyler',
+      },
+      {
+        title: 'Call me by your name',
+        director: 'Luca Guadagnino',
+      },
+      {
+        title: 'Forrest Gump',
+        director: 'Robert Zemeckis',
+      },
+      {
+        title: 'Brokeback Mountain',
+        director: 'Ang Lee',
+      },
+      {
+        title: 'Carol',
+        director: 'Todd Haynes',
+      },
+    ];
+
 http.createServer((request, response) => {
   response.writeHead(200, {'Content-Type': 'text/plain'});
   response.end('Welcome to my book club!\n');
