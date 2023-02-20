f14975f9043dc2f317e78ca9453822e170af171f
tamomsi <122281591+tamomsi@users.noreply.github.com>
2023-02-20 16:20:52 +0100

    importing the express package
    if applied, this commit will import the express package locally and sets up the web server on port 8080

--- index.js
+++ index.js
@@ -1,34 +1,12 @@
+
 const express = require('express');
 const app = express();
 
-let topMovies = [
-    {
-      title: 'Harry Potter and the Sorcerer\'s Stone',
-      director: 'Chris Columbus'
-    },
-    {
-      title: 'Lord of the Rings',
-      director: 'Peter Jackson'
-    },
-    {
-      title: 'Twilight',
-      director: 'Catherine Hardwicke'
-    }
-  ];
-  
-  app.get('/', (req, res) => {
-    res.send('Welcome to my movie club!');
-  });
-  
-  app.get('/documentation', (req, res) => {                  
-    res.sendFile('public/documentation.html', { root: __dirname });
-  });
-  
-  app.get('/movies', (req, res) => {
-    res.json(topMovies);
-  });
-  
-  app.listen(8080, () => {
-    console.log('Your app is listening on port 8080.');
-  });
-  
+const http = require('http');
+
+http.createServer((request, response) => {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end('Welcome to my book club!\n');
+}).listen(8080);
+
+console.log('My first Node test server is running on Port 8080.');
