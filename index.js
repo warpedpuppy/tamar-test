7232e6e287669d687ce8674db6ccdc5109e9567c
tamomsi <122281591+tamomsi@users.noreply.github.com>
2023-02-20 15:44:07 +0100

    added express get route
    if applied, this commit will add express get routs to the file

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
 const express = require('express');
 const app = express();
+
+let topMovies = [
+    {
+      title: 'Harry Potter and the Sorcerer\'s Stone',
+      director: 'Chris Columbus'
+    },
+    {
+      title: 'Lord of the Rings',
+      director: 'Peter Jackson'
+    },
+    {
+      title: 'Twilight',
+      director: 'Catherine Hardwicke'
+    }
+  ];
+  
+  app.get('/', (req, res) => {
+    res.send('Welcome to my movie club!');
+  });
+  
+  app.get('/documentation', (req, res) => {                  
+    res.sendFile('public/documentation.html', { root: __dirname });
+  });
+  
+  app.get('/movies', (req, res) => {
+    res.json(topMovies);
+  });
+  
+  app.listen(8080, () => {
+    console.log('Your app is listening on port 8080.');
+  });
+  
