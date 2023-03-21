023f88e7c8fd2c666961d8401815399790687647
tamomsi <122281591+tamomsi@users.noreply.github.com>
2023-03-21 14:03:22 +0100

    add schema and model code
    if applied, this will add schema and models to the file

--- models.js
+++ models.js
@@ -1,1 +1,32 @@
 const mongoose = require('mongoose');
+
+let movieSchema = mongoose.Schema({
+    Title: {type: String, required: true},
+    Description: {type: String, required: true},
+    Genre: {
+      Name: String,
+      Description: String
+    },
+    Director: {
+      Name: String,
+      Bio: String,
+      Birth: Date,
+      Death: Date
+    },
+    ImagePath: String,
+    Featured: Boolean
+  });
+  
+  let userSchema = mongoose.Schema({
+    UserName: {type: String, required: true},
+    Email: {type: String, required: true},
+    Password: {type: String, required: true},
+    FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }],
+    Birthday: Date
+  });
+  
+  let Movie = mongoose.model('Movie', movieSchema);
+  let User = mongoose.model('User', userSchema);
+  
+  module.exports.Movie = Movie;
+  module.exports.User = User;
