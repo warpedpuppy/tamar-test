3eac3894523765ff1bdf01434a4fc1ca3fb87eb5
tamomsi <122281591+tamomsi@users.noreply.github.com>
2023-03-22 14:22:41 +0100

    get movies by title
    if applied, this commit will add a code to get movies by title

--- index.js
+++ index.js
@@ -24,14 +24,28 @@ app.get('/', (req, res) => {
    res.send('Welcome to myFlix!');
 });
 
+//return JSON object for movies
 app.get('/Movies', (req, res) => {
-  res.json(movies);
+  Movies.find()
+  .then((movies) => {
+    res.status(201).json(movies);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
+  });
 });
 
-app.get('/movies/:title', (req, res) => {
-  res.json(movies.find((movie) =>
-    { return movie.title === req.params.title 
-    }));
+//get movies by title
+app.get('/movies/:Title', (req, res) => {
+  Movies.findOne({ Title: req.params.Title })
+  .then((movie) => {
+    res.json(movie);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('error: ' + err);
+  });
 });
 
 app.get('/genre/:genreName', (req, res) => {
@@ -122,10 +136,10 @@ app.put('/users/:id', (req, res) => {
 });
 
 // Update a user's info, by username
-app.put('/users/:Username', (req, res) => {
-  Users.findOneAndUpdate({ Username: req.params.Username }, { $set:
+app.put('/users/:UserName', (req, res) => {
+  Users.findOneAndUpdate({ UserName: req.params.UserName }, { $set:
     {
-      Username: req.body.Username,
+      UserName: req.body.UserName,
       Password: req.body.Password,
       Email: req.body.Email,
       Birthday: req.body.Birthday
@@ -143,38 +157,36 @@ app.put('/users/:Username', (req, res) => {
 });
 
 //add a new movie to favs
-app.patch('/users/:id/movies/:MovieId', (req, res) => {
-  const { MovieId} = req.params.id;
-
-  let user = users.find((user) => { return user.id === req.params.id });
-
-  if (user) {
-    user.favoriteMovie.push(MovieId);
-    res.status(200).send(
-      `Succesfully added to your favourites`
-    );
-  } else {
-    res.status(400).send('User not found');
-  }
+app.post('/users/:UserName/movies/:MovieID', (req, res) => {
+  Users.findOneAndUpdate({ UserName: req.params.UserName }, {
+     $push: { FavoriteMovies: req.params.MovieID }
+   },
+   { new: true }, 
+  (err, updatedUser) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    } else {
+      res.json(updatedUser);
+    }
+  });
 });
 
-
-app.delete('/users/:id/movies/:MovieId', (req, res) => {
-  const { id, movieName } = req.params;
-
-  let user = users.find((user) => user.id == id);
-
-  if (user) {
-    user.favoriteMovie = user.favoriteMovie.filter(
-      (title) => title !== movieName
-    );
-    res.status(200).send(
-      `Deleted from your Favorites`
-    );
-  } else {
-    res.status(400).send('User not found');
-  }
-});
+//remove a movie from their list of favorites
+app.delete('/users/:UserName/movies/:MovieID', (req, res) => {
+  Users.findOneAndUpdate({ UserName: req.params.UserName }, {
+     $pull: { FavoriteMovies: req.params.MovieID }
+   },
+   { new: true },
+  (err, updatedUser) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    } else {
+      res.json(updatedUser);
+    }
+  });
+ });
 
 app.delete('/users/:id', (req, res) => {
   let user = users.find((user) => { return user.id === req.params.id });
@@ -183,6 +195,22 @@ app.delete('/users/:id', (req, res) => {
     user = users.filter((obj) => { return obj.id !== req.params.id });
     res.status(201).send('user ' + req.params.id + ' was deleted.');
   }
+});
+
+// Delete a user by username
+app.delete('/users/:UserName', (req, res) => {
+  Users.findOneAndRemove({ UserName: req.params.UserName })
+    .then((user) => {
+      if (!user) {
+        res.status(400).send(req.params.UserName + ' was not found');
+      } else {
+        res.status(200).send(req.params.UserName + ' was deleted.');
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
       
 app.get('/documentation', (req, res) => {
