0ab3e3eff3b4fcc8e8d8e4526ba75bce317218fe
tamomsi <122281591+tamomsi@users.noreply.github.com>
2023-03-28 21:11:11 +0200

    Update auth.js
    If applied, this commit will update the code for local strategy

--- auth.js
+++ auth.js
@@ -1,4 +1,5 @@
 const jwtSecret = 'your_jwt_secret'; //the same key used in the JWTStrategy
+
 const jwt = require('jsonwebtoken'),
   passport = require('passport');
 
@@ -13,21 +14,24 @@ let generateJWTToken = (user) => {
 }
 
 module.exports = (router) => {
-  router.post('/login', (req, res) => {
-    passport.authenticate('local', { session: false }, (error, user, info) => {
-      if (error || !user) {
-        return res.status(400).json({
-          message: 'Something is not right',
-          user: user
+    router.post('/login', (req, res) => {
+        passport.authenticate('local',{ session: false }, (error, user, info) => {
+                console.log({error, user})
+                if (error || !user) {
+                    return res.status(400).json({
+                        message: 'Something is not right',
+                        user: user,
+                        error
+                    });
+                }
+                req.login(user, { session: false }, (error) => {
+                    if (error) {
+                        res.send(error);
+                    }
+                    let token = generateJWTToken(user.toJSON());
+                    return res.json({ user, token });
+                });
+            })(req, res);
         });
-      }
-      req.login(user, { session: false }, (error) => {
-        if (error) {
-          res.send(error);
-        }
-        let token = generateJWTToken(user.toJSON());
-        return res.json({ user, token });
-      });
-    })(req, res);
-  });
 }
+
