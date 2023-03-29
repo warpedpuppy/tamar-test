85fdc3a20224309c67e96a756d4d6818095fc633
tamomsi <122281591+tamomsi@users.noreply.github.com>
2023-03-29 11:55:19 +0200

    adding comments to the auth file
    if applied, this commit will add comments to the file

--- auth.js
+++ auth.js
@@ -3,16 +3,17 @@ const jwtSecret = 'your_jwt_secret'; //the same key used in the JWTStrategy
 const jwt = require('jsonwebtoken'),
   passport = require('passport');
 
-require('./passport'); 
+require('./passport');  // Your local passport file
 
 let generateJWTToken = (user) => {
   return jwt.sign(user, jwtSecret, {
-    subject: user.UserName, 
-    expiresIn: '7d', 
-    algorithm: 'HS256' 
+    subject: user.UserName, // This is the username you’re encoding in the JWT
+    expiresIn: '7d', // This specifies that the token will expire in 7 days
+    algorithm: 'HS256' // This is the algorithm used to “sign” or encode the values of the JWT
   });
 }
 
+//post login
 module.exports = (router) => {
     router.post('/login', (req, res) => {
         passport.authenticate('local',{ session: false }, (error, user, info) => {
